fde35e10c6b674c779500865372bddb292417aae
moe11elf <moritzalsmoe@gmx.net>
2015-12-16 19:44:55 +0100

    shader added

--- src/executables/tessellation/main.cpp
+++ src/executables/tessellation/main.cpp
@@ -33,6 +33,8 @@ static GLuint s_texHandle = 0;
 
 static std::vector<float> s_vectorTexData;
 static const int VECTOR_TEXTURE_SIZE = 128;
+static float IndexCount;
+
 void updateVectorTexture(double elapsedTime)
 {
 	// first time, create it
@@ -79,6 +81,71 @@ void updateVectorTexture(double elapsedTime)
 	}
 }
 
+static void CreateIcosahedron() {
+	    const int Faces[] = {
+        2, 1, 0,
+        3, 2, 0,
+        4, 3, 0,
+        5, 4, 0,
+        1, 5, 0,
+        11, 6, 7,
+        11, 7, 8,
+        11, 8, 9,
+        11, 9, 10,
+        11, 10, 6,
+        1, 2, 6,
+        2, 3, 7,
+        3, 4, 8,
+        4, 5, 9,
+        5, 1, 10,
+        2,  7, 6,
+        3,  8, 7,
+        4,  9, 8,
+        5, 10, 9,
+        1, 6, 10 };
+
+    const float Verts[] = {
+         0.000f,  0.000f,  1.000f,
+         0.894f,  0.000f,  0.447f,
+         0.276f,  0.851f,  0.447f,
+        -0.724f,  0.526f,  0.447f,
+        -0.724f, -0.526f,  0.447f,
+         0.276f, -0.851f,  0.447f,
+         0.724f,  0.526f, -0.447f,
+        -0.276f,  0.851f, -0.447f,
+        -0.894f,  0.000f, -0.447f,
+        -0.276f, -0.851f, -0.447f,
+         0.724f, -0.526f, -0.447f,
+         0.000f,  0.000f, -1.000f };
+
+    IndexCount = sizeof(Faces) / sizeof(Faces[0]);
+ 
+    // Create the VAO:
+    GLuint vao;
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
+
+    // Create the VBO for positions:
+    GLuint positions;
+    
+	// added by me
+	GLuint slot;
+
+    GLsizei stride = 3 * sizeof(float);
+    glGenBuffers(1, &positions);
+    glBindBuffer(GL_ARRAY_BUFFER, positions);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(Verts), Verts, GL_STATIC_DRAW);
+    glEnableVertexAttribArray(slot);
+    glVertexAttribPointer(slot, 3, GL_FLOAT, GL_FALSE, stride, 0);
+
+    // Create the VBO for indices:
+    GLuint indices;
+    glGenBuffers(1, &indices);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indices);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(Faces), Faces, GL_STATIC_DRAW);
+}
+
+
 int main()
 {
 	DEBUGLOG->setAutoPrint(true);
@@ -115,11 +182,11 @@ int main()
 
 	/////////////////////// 	Renderpass     ///////////////////////////
 	DEBUGLOG->log("Shader Compilation: volume uvw coords"); DEBUGLOG->indent();
-	ShaderProgram shaderProgram("/modelSpace/GBuffer.vert", "/modelSpace/GBuffer.frag"); DEBUGLOG->outdent();
-	shaderProgram.update("model", model);
-	shaderProgram.update("view", view);
-	shaderProgram.update("projection", perspective);
-	shaderProgram.update("color", glm::vec4(1.0f,0.0f,0.0f,1.0f));
+	ShaderProgram shaderProgram("/tessellation/tess.vert", "/tessellation/tess.frag"); DEBUGLOG->outdent();
+	//shaderProgram.update("model", model);
+	//shaderProgram.update("view", view);
+	//shaderProgram.update("projection", perspective);
+	//shaderProgram.update("color", glm::vec4(1.0f,0.0f,0.0f,1.0f));
 
 	DEBUGLOG->log("FrameBufferObject Creation: volume uvw coords"); DEBUGLOG->indent();
 	FrameBufferObject fbo(getResolution(window).x, getResolution(window).y);
---
+++ src/shaders/tessellation/tess.vert
@@ -0,0 +1,7 @@
+in vec4 Position;
+out vec3 vPosition;
+
+void main()
+{
+    vPosition = Position.xyz;
+}
