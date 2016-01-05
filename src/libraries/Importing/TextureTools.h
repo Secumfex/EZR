5ab69d2d97a8f52ffa815c79ea805b033745919c
Secumfex <Arend.Buchacher@gmx.de>
2016-01-05 16:57:32 +0100

    #1 added missing define guard

--- src/libraries/Importing/TextureTools.h
+++ src/libraries/Importing/TextureTools.h
@@ -1,3 +1,6 @@
+#ifndef TEXTURE_TOOLS_H
+#define TEXTURE_TOOLS_H
+
 #include <string>
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
@@ -10,3 +13,5 @@ namespace TextureTools {
  	 */
     GLuint loadTexture(std::string fileName);
 }
+
+#endif
