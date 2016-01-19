f068faeceae937eb2051807504bdd1b1f4383578
Secumfex <Arend.Buchacher@gmx.de>
2016-01-19 17:14:49 +0100

    #7 #1 random tree phase

--- src/libraries/TreeAnimation/Tree.cpp
+++ src/libraries/TreeAnimation/Tree.cpp
@@ -130,7 +130,8 @@ TreeAnimation::Tree* TreeAnimation::Tree::generateTree(float approxHeight, float
 	float rThickness = rWidth - abs(approxWidth - rWidth);
 
 	float rHeight = approxHeight + ((float) rand() / (float) RAND_MAX) * (0.1f * approxHeight) - (0.05f * approxHeight); //random offset of 10%
-	TreeAnimation::Tree* tree = new Tree(rHeight, rWidth,rThickness, E_RED_OAK);
+	float rPhase = ((float) rand() / (float) RAND_MAX) * (glm::pi<float>()) - (glm::half_pi<float>());
+	TreeAnimation::Tree* tree = new Tree(rHeight, rWidth,rThickness, E_RED_OAK, rPhase);
 	
 	if ( branchPtrs != nullptr){ branchPtrs->push_back(&tree->m_trunk); }
 	
